37242490fc4293067cfdce34ec826691c33515a1
bhara <vinodb@shernet.sheridancollege.ca>
2025-10-07 16:22:02 -0400

    testing

--- src/main/java/com/mycompany/syst17796_unoproject/UnoGameTest.java
+++ src/main/java/com/mycompany/syst17796_unoproject/UnoGameTest.java
@@ -19,6 +19,6 @@ public class UnoGameTest {
             }
         }
         
-        System.out.println("\nRemaining cards in deck: " + unoDeck.size());
+        System.out.println("\nRemaining cards in deck test: " + unoDeck.size());
     }
 }
